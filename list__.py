0eb1f2be1ece398f79421853f14ed99b5d092bc9
MichaelMigachev <migachev21@gmail.com>
2026-05-12 13:24:31 +0300

    3.2

--- list__.py
+++ list__.py
@@ -2,3 +2,8 @@
 # print(hp_books[2])
 # print(hp_books[0])
 
+nums = ["zero", "first", "second", "third", "fourth", "fifth"]
+# print(nums[:])
+# print(nums[:3])
+# print(nums[1:2])
+print(nums[3:])
